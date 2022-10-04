b40da4fe0d4621c4671877f89998b53aa37c0d1e
BarbaRadoBeUniq <xl_alliance@mail.ru>
2022-10-04 15:29:15 +0600

    hw7

---
+++ dz7.py
@@ -0,0 +1,85 @@
+#ДЗ 7
+'''
+1. Написать функцию binary_search, принимающую в качестве входящего параметра
+элемент для поиска и список в котором необходимо искать.
+2. Алгоритм должен искать с помощью двоичного поиска, изображенного на блок-схеме
+презентации.
+3. Функция в итоге должна распечатать результат.
+4. Написать функцию buble_sort или selection_sort, принимающую в качестве входящего
+параметра не отсортированный список.
+5. Алгоритм функции должен сортировать список методом пузырьковой сортировки или
+методом сортировки выбором.
+6. Функция в итоге должна возвращать отсортированный список.
+'''
+# 1111111
+
+'''
+def binary_search(a, list):
+    list.sort()
+    print(list)
+    first = 0  
+    length = len(list)
+    last = length - 1  
+    middle_index = (first + last) // 2  
+    middle = list[middle_index] 
+
+    while True:
+        if first == last:
+            if middle != a:
+                print(f'Число {a} не найдено в данном списке')
+                break
+
+        elif middle == a:
+            print(f"Число {a} найдено в списке под индексом {middle_index}")
+            break
+
+        elif middle > a:
+            high = middle_index - 1
+            last = high
+            middle_index = (first + last) // 2
+            middle = list[middle_index]
+            if middle == a:
+                print(f"Число {a} найдено в списке под индексом {middle_index}")
+                break
+
+        elif middle < a:
+            low = middle_index + 1
+            first = low
+            last = length - 1
+            middle_index = (first + last) // 2
+            middle = list[middle_index]
+            if middle == a:
+                print(f"Число {a} найдено в списке под индексом {middle_index}")
+                break
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+binary_search(3, list)
+
+'''
+
+# 2222222222
+
+'''
+
+def bubble_sort(a):
+    size = len(a)
+    step = 0
+    index = size - 1
+    for i in range(index):
+        for b in range(0, index - i):
+            if a[b] > a[b + 1]:
+                step = a[b]
+                a[b] = a[b + 1]
+                a[b + 1] = step
+
+    print(a)
+
+
+list = [1, 7, 3, 4, 9, 8, 11, 10, 5, 2, 6]
+bubble_sort(list)
+
+'''
---
+++ test.py
@@ -0,0 +1,76 @@
+
+
+# list = [5, 2, 7, 1, 3]
+# list2 = []
+# c = 0
+# len = len(list)
+
+# for i in range(len):
+#     for b in list:
+#         if b > list[i]:
+#             list2.append(b)
+
+# print(list2)
+
+
+
+
+# list = [5, 6, 7, 8, 9, 10, 11]
+# first = 0
+# c = len(list) - 1
+# last = list[c]
+# length = len(list)
+# middle = length // 2
+# val = list[middle]
+# length = len(list)
+
+# # print(c)
+# a = 13
+
+# while True:
+
+    
+
+    
+#     if a == val:
+#         print(f"Число {a} найдено, индекс: {list.index(val)}")
+#         break
+     
+
+#     if a < val:
+#         c = middle 
+#         middle = (first + c) // 2
+#         val = list[middle]
+        
+#         continue
+    
+
+#     if a > val:
+#         first = middle 
+#         middle = ((first + c) // 2 ) + 1
+#         val = list[middle]
+        
+#         continue
+
+#     else:
+#         first == last
+#         if middle != a:
+#                 print("No")
+#                 break
+
+    
+
+   
+
+            
+
+
+
+
+
+
+
+
+
+
+
